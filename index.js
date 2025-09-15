385b56f56e674203e687dd84bb32ef8d4cf8e747
hariharan-dev-05 <hariharandeveloper05@gmail.com>
2025-09-15 21:55:29 +0530

    CORS fixed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://snipr-server-production.up.railway.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
