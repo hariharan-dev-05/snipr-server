931402d8f2d42d375bd9b7e9bfea0a84994a6b84
hariharan-dev-05 <hariharandeveloper05@gmail.com>
2025-09-16 16:20:02 +0530

    db connection fixed

--- db.js
+++ db.js
@@ -1,10 +1,8 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// Connect to MongoDB using the URI from .env
+mongoose.connect(process.env.MONGODB_URI);
 
 const db = mongoose.connection;
 
